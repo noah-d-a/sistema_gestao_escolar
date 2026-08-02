3ec568eab9d61cc1a6a34e42a70584e56e901522
Noah Duarte Alves <noahduartealves@gmail.com>
2026-08-02 19:05:12 -0300

    Atualização do conexao.php
    Foram adicionados:
    1. Definição de data e hora
    2. Criação da conexão
    3. Checagem para caso a conexão não tenha sido formada
    4. Definição do conjunto de caracteres

--- sistema_gestao_escolar/php/conexao.php
+++ sistema_gestao_escolar/php/conexao.php
@@ -1,3 +1,11 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
 
+$conexao = new mysqli("localhost", "root", "", "sistema_gestao_escolar");
+
+if ($conexao->connect_error) {
+    die("Erro de conexão: " . $conexao->connect_error);
+}
+
+$conexao->set_charset($conexao, "utf8mb4");
 ?>
