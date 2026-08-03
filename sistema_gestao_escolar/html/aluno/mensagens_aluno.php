3f3df8ab19263317a931816326e08c0e2544e460
maahbas <mariliacboliveira@gmail.com>
2026-08-03 20:10:06 -0300

    mensagens

--- sistema_gestao_escolar/html/aluno/mensagens_aluno.php
+++ sistema_gestao_escolar/html/aluno/mensagens_aluno.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>SGE - Mensagens</title>
 
+    <link rel="stylesheet" href="../../css/padronizacao.css"/>
+</head>
+<body>
+
+    <?php include '../../includes/menu_aluno.php'; ?>
+
+    <main>
+        <h1>Mensagens</h1>
+        <p>Confira suas mensagens recebidas.</p>
+
+        <div class="tabela">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Remetente</th>
+                        <th>Assunto</th>
+                        <th>Data</th>
+                        <th>Situação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><td>Janaina</td><td>Prova de Matemática</td><td>01/08/2026</td><td>Não lida</td></tr>
+                    <tr><td>Tia larakkk</td><td>Reunião de pais</td><td>30/07/2026</td><td>Lida</td></tr>
+                    <tr><td>Mecha</td><td>Trabalho em grupo</td><td>28/07/2026</td><td>Lida</td></tr>
+                    <tr><td>Coordenação</td><td>Aviso importante</td><td>25/07/2026</td><td>Lida</td></tr>
+                </tbody>
+            </table>
+        </div>
+
+    </main>
+
+</body>
+</html>
