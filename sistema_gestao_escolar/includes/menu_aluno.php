67aa4672dfa22d9d0db6ad6b00ae58531363b17a
Marília <mariliacboliveira@gmail.com>
2026-08-03 18:10:16 -0300

    Update menu_aluno.php

--- sistema_gestao_escolar/includes/menu_aluno.php
+++ sistema_gestao_escolar/includes/menu_aluno.php
@@ -1,37 +1,27 @@
 <nav id="menu">
     <ul>
         <li>
-            <a href="../php/aluno/inicio_aluno.php">
+            <a href="../aluno/inicio_aluno.php">
                 <p>Início</p>
             </a>
         </li>
-
         <li>
-            <a href="../php/aluno/cadastro_aluno.php">
-                <p>Cadastro</p>
-            </a>
-        </li>
-
-        <li>
-            <a href="../php/aluno/boletim_aluno.php">
+            <a href="../aluno/boletim_aluno.php">
                 <p>Boletim</p>
             </a>
         </li>
-
         <li>
-            <a href="../php/aluno/presenca_aluno.php">
+            <a href="../aluno/presenca_aluno.php">
                 <p>Presença</p>
             </a>
         </li>
-
         <li>
-            <a href="../php/aluno/horario_aluno.php">
+            <a href="../aluno/horario_aluno.php">
                 <p>Horário</p>
             </a>
         </li>
-
         <li>
-            <a href="../php/aluno/mensagens_aluno.php">
+            <a href="../aluno/mensagens_aluno.php">
                 <p>Mensagens</p>
             </a>
         </li>
